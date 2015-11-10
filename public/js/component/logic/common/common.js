270ee60d18c3df8ae55942269af848ef5fbc5fb1
xiongxiong109 <444122662@qq.com>
2015-11-10 18:25:22 +0800

    导航条 用户信息card

--- public/js/component/logic/common/common.js
+++ public/js/component/logic/common/common.js
@@ -1,14 +1,4 @@
 define(function(require,exports,module){
-	//公共部分js
-
-	//全局搜索部分
-	$(".search-btn").on('click',function(){
-		var searchStr=$(".search-input").val();
-		if( !searchStr ){
-			window.location.reload();
-		}
-		else{
-			window.location.href="/search?word="+searchStr;
-		}
-	});
+	//全局搜索
+	var suggest=require('/js/component/base/suggest/suggest');
 });
