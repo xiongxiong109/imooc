5de3f22d2e02dbc49754ea7c7cb075af8282966c
xiongxiong109 <444122662@qq.com>
2015-11-13 18:10:11 +0800

    完成首页整体效果

--- public/js/page/index/index.js
+++ public/js/page/index/index.js
@@ -8,7 +8,7 @@ define(function(require,exports,module){
 		doms:null,
 		interval:4000,//自动播放延时
 		isRunning:false,
-		init:function(){
+		init:function(){ 
 			var s=this;
 			$dom=s.getDom();
 			s.doms=$dom;
@@ -98,23 +98,21 @@ define(function(require,exports,module){
 
 		for(var i=0;i<oArr.length;i++){
 			oArr[i].index=i;
-		}
-		$(window).on('scroll',checkScroll);
-		for(var i=0;i<oArr.length;i++){
-			$(window).trigger('scroll');
+			oArr[i].isRun=false;
 		}
 		//检测滚动
+		$(window).on('scroll',checkScroll);
+		$(window).trigger('scroll');
 		function checkScroll(){
 			var scrollTop=$(window).height()+$(window).scrollTop();
-			if(!oArr.length){
-				$(window).off('scroll',checkScroll);
-			}
-			else{
-				for(var i=0;i<oArr.length;i++){
-					if(scrollTop>=$(oArr[i]).offset().top+$(oArr[i]).height()/2){
+			for(var i=0;i<oArr.length;i++){
+				if(scrollTop>=$(oArr[i]).offset().top+$(oArr[i]).height()/8){
+					if(!oArr[i].isRun){
+						if(i==oArr.length){
+							$(window).off('scroll',checkScroll);
+						}
+						oArr[i].isRun=true;
 						runAnimate(oArr[i]);
-						oArr.splice(i,1);
-						break;
 					}
 				}
 			}
@@ -126,14 +124,8 @@ define(function(require,exports,module){
 					var oImgs=$(obj).find('img');
 					oImgs.css('position','relative');
 					oImgs.each(function(idx,ele){
-						$(ele).css({
-							'top':10+8*idx,
-							'opacity':0
-						});
-						$(ele).delay(idx*200).animate({
-							'top':0,
-							'opacity':1
-						},500,'swing');
+						$(ele).css({'top':10+8*idx,'opacity':0});
+						$(ele).delay(idx*200).animate({'top':0,'opacity':1},500,'swing');
 					});break;
 				case 1:
 					var $star=$(obj).find('.star');
@@ -151,7 +143,87 @@ define(function(require,exports,module){
 					.delay(100)
 					.animate({'opacity':1,'top':-31},600);
 					break;
+				case 2:
+					var cmp=$(obj).find('.computer');
+					var txt=$(obj).find('.text2');
+					cmp.css({'top':0}).animate({'opacity':1,'top':-26},400,'swing');
+					txt.delay(80).animate({'opacity':1},600);
+					break;
+				case 3:
+					var calendar=$(obj).find('.calendar');
+					var rocket=$(obj).find('.rockets');
+					var smoke=$(obj).find('.smoke');
+					var txt=$(obj).find('.text3');
+					txt.animate({'opacity':1},400);
+					calendar.animate({'opacity':1},400);
+					rocket.css('top',0).delay(100).animate({'opacity':1 ,'top':-46},300);
+					smoke.delay(350).animate({'opacity':1},400);
+					break;
+				case 4:
+					var txt=$(obj).find('.text4');
+					var hand=$(obj).find('.hand');
+					var icon=$(obj).find('.icon');
+					txt.animate({'opacity':1});
+					hand.css('top',0).delay(80).animate({'opacity':1,'top':-30},400,'swing');
+					icon.delay(300).animate({'opacity':1},400,'swing');
+					break;
+				case 5:
+					var btn=$(obj).find('.join-btn');
+					btn.css({'position':'relative','top':50,'opacity':0})
+					.delay(200)
+					.animate({'top':0,'opacity':1},400,'swing');
 			}
 		}
+	})();
+
+	//慕课大家庭
+	(function(){
+		var oWrap=$(".mooc-family");
+		var lgMask=$(".family-mask");
+		var lgTxt=$(".family-text");
+		var wTimer=null;
+		//wrap
+		oWrap.on('mouseenter',function(){
+			wTimer=setTimeout(function(){
+				lgMask.fadeOut(200);
+				lgTxt.fadeOut(200);
+			},200);
+		})
+		.on('mouseleave',function(){
+			clearTimeout(wTimer);
+			lgMask.fadeIn(200);
+			lgTxt.fadeIn(200);
+		});
+
+		//li
+		var oHoverLis=$(".family-teacher, .student-list li");
+		oHoverLis.each(function(idx,ele){
+			ele.timer=null;
+			var msk=$(ele).find('.list-mask');
+			var info=$(ele).find('.family-info');
+			$(ele).on('mouseenter',function(){
+				msk.fadeOut(200);
+				ele.timer=setTimeout(function(){
+					info.fadeIn();
+				},800);
+			})
+			.on('mouseleave',function(){
+				msk.fadeIn(200);
+				info.fadeOut();
+				clearTimeout(ele.timer);
+			});
+
+		});
+	})();
+
+	//安卓二维码
+	(function(){
+		$(".download-barcode").hover(function(){
+			$(".slide-code").css({'bottom':116,'opacity':1});
+			$(".slide-mobile").css({'bottom':-14,'opacity':0});
+		},function(){
+			$(".slide-code").css({'bottom':86,'opacity':0});
+			$(".slide-mobile").css({'bottom':0,'opacity':1});
+		});
 	})();
 });
